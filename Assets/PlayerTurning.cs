2e33e3aeb4c64da4909207e37b9a262668dd5f90
Crucible70 <cmagzz70@gmail.com>
2023-03-11 22:08:20 +0000

    Initial Upload 2

---
+++ Assets/PlayerTurning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTurning : MonoBehaviour
+{
+    float xRotation = 0f;
+    public Vector2 turn;
+    public float sensitivity = 50;
+    public Vector3 deltaMove;
+    public float speed = 1;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        turn.y = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= turn.y;
+        xRotation = Mathf.Clamp(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, turn.x, 0);
+    }
+}
